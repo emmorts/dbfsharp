1aeb77c1bf2ccd233126fce183749b5145fd98c3
Tomas Stropus <h.guzas@gmail.com>
2025-07-04 22:50:24 +0300

    refactor: remove unimplemented export and validate commands
    Clean up command registration by removing placeholder export and validate commands that were not yet implemented. Simplify Program.cs configuration and use assembly version for application versioning.

--- DbfSharp.Console/Program.cs
+++ DbfSharp.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Reflection;
+using System.Text;
 using DbfSharp.Console.Commands;
 using Spectre.Console;
 using Spectre.Console.Cli;
@@ -21,35 +22,25 @@ try
     app.Configure(config =>
     {
         config.SetApplicationName("dbfsharp");
-        config.SetApplicationVersion("1.0.0");
+        config.SetApplicationVersion(Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "Unknown");
                 
-        // Add commands
         config.AddCommand<ReadCommand>("read")
             .WithDescription("Read and display DBF file contents")
             .WithExample("read", "data.dbf")
             .WithExample("read", "data.dbf", "--format", "csv")
-            .WithExample("read", "data.dbf", "--limit", "100", "--fields", "NAME,SALARY");
+            .WithExample("read", "data.dbf", "--limit", "100", "--fields", "NAME,SALARY")
+            .WithExample("read");
                 
         config.AddCommand<InfoCommand>("info")
             .WithDescription("Display DBF file information and structure")
             .WithExample("info", "data.dbf")
-            .WithExample("info", "data.dbf", "--verbose");
-                
-        config.AddCommand<ExportCommand>("export")
-            .WithDescription("Export DBF file to various formats")
-            .WithExample("export", "data.dbf", "--output", "data.csv")
-            .WithExample("export", "data.dbf", "--format", "json", "--output", "data.json");
-                
-        config.AddCommand<ValidateCommand>("validate")
-            .WithDescription("Validate DBF file structure and integrity")
-            .WithExample("validate", "data.dbf");
+            .WithExample("info", "data.dbf", "--verbose")
+            .WithExample("info");
 
-        // Use Spectre.Console for help
         config.UseStrictParsing();
         config.ValidateExamples();
     });
 
-    // Run the application
     return await app.RunAsync(args);
 }
 catch (Exception ex)
